575c30a7ee2c97db1aea935b9beef0ddfa0f98a1
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2017-06-19 17:17:44 +0530

    Update Little Chef and Numbers.c

--- C Programs/C Programs - 6/Little Chef and Numbers.c
+++ C Programs/C Programs - 6/Little Chef and Numbers.c
@@ -2,21 +2,22 @@
 
 void solve()
 {
-    int number_of_non_1_non_0 = 0, number_of_2 = 0, number_i, i, number_of_elements;
+    int number_of_greater_than_1 = 0, number_of_2 = 0, number_i, i, number_of_elements;
     long long no_of_pairs;
     scanf("%d", &number_of_elements);
 
     for(i = 1; i <= number_of_elements; i++)
     {
         scanf("%d", &number_i);
-        number_of_non_1_non_0 += (number_i > 1);
+        number_of_greater_than_1 += (number_i > 1);
         number_of_2 += (number_i == 2);
     }
 
-    no_of_pairs = (number_of_non_1_non_0*1LL*(number_of_non_1_non_0 - 1) )/2;
-    no_of_pairs -= (number_of_2*1LL*(number_of_2 - 1) )/2;
+    if(number_of_2 == number_of_greater_than_1)
+        no_of_pairs = 0;
+    else
+        no_of_pairs = (number_of_greater_than_1*1LL*(number_of_greater_than_1 - 1) )/2 - (number_of_2*1LL*(number_of_2 - 1) )/2;
 
-    if(number_of_2 == number_of_non_1_non_0) no_of_pairs = 0;
     printf("%lld\n", no_of_pairs);
 }
 
