067a75af98b5162240ede860a39496eae86f6f15
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2017-06-22 23:01:28 +0530

    Update Cats_and_Dogs.c

--- C Programs/C Programs - 1/Cats_and_Dogs.c
+++ C Programs/C Programs - 1/Cats_and_Dogs.c
@@ -6,9 +6,14 @@
 int is_counting_correct()
 {
     int no_of_cats, no_of_dogs, no_of_legs, correct_counting = NO;
+    long long cats_on_floor, cats_on_back;
     scanf("%d %d %d", &no_of_cats, &no_of_dogs, &no_of_legs);
 
-    if(no_of_legs%4 == 0 && no_of_legs >= 4*no_of_dogs && no_of_legs <= 4*(no_of_cats + no_of_dogs))
+    cats_on_floor = (no_of_legs - 4*no_of_dogs)/4;
+    cats_on_back = no_of_cats - cats_on_floor;
+
+    correct_counting = NO;
+    if(no_of_legs%4 == 0 && no_of_legs >= 4LL*no_of_dogs && no_of_legs <= 4LL*(no_of_cats + no_of_dogs) && cats_on_back <= 2LL*no_of_dogs)
     {
         correct_counting = YES;
     }
