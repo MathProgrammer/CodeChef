fda32c1e51552bd6d99029b3867769d46b7d7fb7
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2017-06-23 09:44:20 +0530

    Add files via upload

---
+++ C Programs/C Programs - 7/Devu and Friendship Testing Alternate Solution.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unordered_set> //Needs C++ 11 or above
+
+using namespace std;
+
+void solve()
+{
+    int no_of_friends;
+    unordered_set <int> saved_friendships;
+    scanf("%d", &no_of_friends);
+
+    for(int i = 1; i <= no_of_friends; i++)
+    {
+        int day_i;
+        scanf("%d", &day_i);
+
+        saved_friendships.insert(day_i);
+    }
+
+    printf("%d\n", saved_friendships.size());
+}
+
+int main()
+{
+    int no_of_test_cases;
+    scanf("%d", &no_of_test_cases);
+
+    while(no_of_test_cases--)
+        solve();
+
+    return 0;
+}
+
