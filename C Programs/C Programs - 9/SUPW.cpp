824785614a0b5e981c04bcce7da90a01b7f6e438
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2018-01-22 17:38:41 +0530

    Update SUPW.cpp

--- C Programs/C Programs - 9/SUPW.cpp
+++ C Programs/C Programs - 9/SUPW.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <vector>
 
+#define min_3(a, b, c) min(a, min(b, c))
 using namespace std;
 
 int main()
@@ -8,30 +9,25 @@ int main()
     int no_of_days;
     scanf("%d", &no_of_days);
 
-    vector <int> time(no_of_days + 5, 0);
+    vector <int> time(no_of_days + 1);
     for(int i = 1; i <= no_of_days; i++)
         scanf("%d", &time[i]);
 
-    const int WORK = 0, NO_WORK = 1;
-    int minimum_time_starting[no_of_days + 5][2];
-
+    vector <int> minimum_time_starting(no_of_days + 1, 0);
 
     for(int i = no_of_days; i >= 1; i--)
     {
-        if(i == no_of_days || i == no_of_days - 1)
+        if(i >= no_of_days - 1)
         {
-            minimum_time_starting[i][WORK] = time[i];
-            minimum_time_starting[i][NO_WORK] = 0;
+            minimum_time_starting[i] = 0;
             continue;
         }
 
-        minimum_time_starting[i][NO_WORK] = min(minimum_time_starting[i + 1][WORK], minimum_time_starting[i + 2][WORK]);
-
-        minimum_time_starting[i][WORK] = time[i] + min(minimum_time_starting[i + 1][WORK], minimum_time_starting[i + 1][NO_WORK]);
+        minimum_time_starting[i] = min_3(time[i] + minimum_time_starting[i + 1], time[i + 1] + minimum_time_starting[i + 2],
+                                         time[i + 2] + minimum_time_starting[i + 3]);
     }
 
-    int total_time = min(minimum_time_starting[1][WORK], minimum_time_starting[1][NO_WORK]);
-    printf("%d\n", total_time);
+    printf("%d\n", minimum_time_starting[1]);
 
     return 0;
 }
